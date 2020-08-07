ca3287d3d8817d501b8692379f6d71c6e3aae691
MengMeng96 <chunmengzhong@gamil.com>
2020-08-07 18:36:15 +0800

    fit

--- frontend/server/src/main/java/org/pytorch/serve/util/ConfigManager.java
+++ frontend/server/src/main/java/org/pytorch/serve/util/ConfigManager.java
@@ -131,7 +131,7 @@ public final class ConfigManager {
                 filePath = getLastSnapshot();
                 if (filePath == null) {
                     if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) {
-                        filePath = System.getProperty("tsConfigFile", "config_win.properties");
+                        filePath = "config_win.properties";
                     } else {
                         filePath = System.getProperty("tsConfigFile", "config.properties");
                     }
