caf9fd85bd4298adb58b1ebc98e1f5c08288230e
MengMeng96 <chunmengzhong@gamil.com>
2020-08-10 17:16:51 +0800

    fit

--- frontend/server/src/test/java/org/pytorch/serve/util/ConfigManagerTest.java
+++ frontend/server/src/test/java/org/pytorch/serve/util/ConfigManagerTest.java
@@ -47,6 +47,7 @@ public class ConfigManagerTest {
     @Test
     public void test() throws IOException, GeneralSecurityException, ReflectiveOperationException {
         modifyEnv("TS_DEFAULT_RESPONSE_TIMEOUT", "130");
+        System.setProperty("TS_DEFAULT_RESPONSE_TIMEOUT", "130");
         System.out.println(System.getenv("TS_DEFAULT_RESPONSE_TIMEOUT"));
         ConfigManager.Arguments args = new ConfigManager.Arguments();
         args.setModels(new String[] {"noop_v0.1"});
