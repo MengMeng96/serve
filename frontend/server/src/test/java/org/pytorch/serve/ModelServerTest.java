88aeab4bde540b9315e61d9a2ae4a4884859bf2a
MengMeng96 <chunmengzhong@gamil.com>
2020-08-11 16:18:40 +0800

    fit

--- frontend/server/src/test/java/org/pytorch/serve/ModelServerTest.java
+++ frontend/server/src/test/java/org/pytorch/serve/ModelServerTest.java
@@ -111,10 +111,10 @@ public class ModelServerTest {
         TestUtils.setLatch(new CountDownLatch(1));
         TestUtils.getRoot(channel);
         TestUtils.getLatch().await();
-        System.out.println(TestUtils.getResult().replaceAll("\n\r", "\r"));
+        System.out.println(TestUtils.getResult().replaceAll("\r", "\n\r"));
         System.out.println("---------------------");
-        System.out.println(listInferenceApisResult.replaceAll("\n\r", "\r"));
-        Assert.assertEquals(TestUtils.getResult().replaceAll("\n\r", "\r")
+        System.out.println(listInferenceApisResult.replaceAll("\r", "\n\r"));
+        Assert.assertEquals(TestUtils.getResult().replaceAll("\r", "\n\r")
                 , listInferenceApisResult);
     }
 
